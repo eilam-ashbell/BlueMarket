726a8d7c8674f9bd295d10db6b95469e35dc93d5
eilam-ashbell <eashbell@gmail.com>
2023-01-07 22:55:17 +0200

    delete empty css files

--- Frontend/src/app/components/auth-area/logout/logout.component.ts
+++ Frontend/src/app/components/auth-area/logout/logout.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent {
 
